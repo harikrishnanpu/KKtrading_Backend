ded6feb09c71a0fed8b779fa291eafb6d0f2ced3
Hari Krishnan <harikrishnan@Haris-MacBook-Air-2.local>
2025-01-31 17:40:23 +0530

    NEW jhbhubh

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 
 
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://hari:1234@kktradingbackend.ip6yq.mongodb.net/?retryWrites=true&w=majority&appName=KKTRADINGBACKEND');
+mongoose.connect('mongodb+srv://hari:1234@kktradingbackend.ip6yq.mongodb.net/?retryWrites=true&w=majority&appName=KKTRADINGBACKEND');
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
